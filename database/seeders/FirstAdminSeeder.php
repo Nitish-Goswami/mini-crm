604896c00f22dd7127ed08445c140b7a4e45d101
Nitish Goswami <ni30.info@gmail.com>
2023-02-04 12:43:01 +0530

    First user created with email admin@admin.com and password 'password' with database seeds

---
+++ database/seeders/FirstAdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FirstAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admin')->insert([
+            'email' => 'admin@admin.com',
+            'password' => 'password',
+        ]);
+    }
+}
